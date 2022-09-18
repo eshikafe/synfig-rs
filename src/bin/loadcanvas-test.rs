f211b54335538b980b8d5bbeb185797294d5a086
eshikafe <eshikafe@gmail.com>
2022-09-18 23:27:57 +0100

    Allow error in test

--- src/bin/loadcanvas-test.rs
+++ src/bin/loadcanvas-test.rs
@@ -11,6 +11,6 @@ canvas.register_canvas_in_map(1, "c://Documents".to_string());
 canvas.show_canvas_map("file".to_string(), 1, "text".to_string());
 
 // Test log functions
-// canvas.set_allow_errors(true);
+canvas.set_allow_errors(true);
 canvas.error(1234, "canvas layer missing".to_string());
 }
