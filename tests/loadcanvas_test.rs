700cee1ef26050cab4ac2405319d8921ac9ee485
eshikafe <eshikafe@gmail.com>
2022-09-21 09:49:34 +0100

    Format loadcanvas_test.rs

--- tests/loadcanvas_test.rs
+++ tests/loadcanvas_test.rs
@@ -2,21 +2,19 @@ use synfig::loadcanvas::*;
 
 #[test]
 fn run() {
+    // Test canvas data structure
+    let mut canvas = CanvasParser::new();
 
-// Test canvas data structure
-let mut canvas = CanvasParser::new();
+    // Test canvas methods
+    canvas.register_canvas_in_map(32, "c://Documents/synfig".to_string());
+    canvas.register_canvas_in_map(1, "c://Documents".to_string());
+    canvas.show_canvas_map("file".to_string(), 1, "text".to_string());
 
-// Test canvas methods
-canvas.register_canvas_in_map(32, "c://Documents/synfig".to_string());
-canvas.register_canvas_in_map(1, "c://Documents".to_string());
-canvas.show_canvas_map("file".to_string(), 1, "text".to_string());
+    assert_eq!(0, canvas.error_count());
+    // Test log functions
+    canvas.set_allow_errors(true);
+    canvas.error(1234, "canvas layer missing".to_string());
+    assert_eq!(1, canvas.error_count());
 
-assert_eq!(0,canvas.error_count());
-// Test log functions
-canvas.set_allow_errors(true);
-canvas.error(1234, "canvas layer missing".to_string());
-assert_eq!(1,canvas.error_count());
-
-canvas.warning(98, "attribute unknown".to_string());
-
+    canvas.warning(98, "attribute unknown".to_string());
 }
